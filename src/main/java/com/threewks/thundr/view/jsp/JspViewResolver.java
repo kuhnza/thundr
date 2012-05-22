0143ece4338c4e68ee2f357e977668930251c936
Nick Okunew <nick@atomicleopard.com>
2012-05-22 15:00:50 +1000

    Nick O: added global model attributes to the jsp view resolver

--- src/main/java/com/threewks/thundr/view/jsp/JspViewResolver.java
+++ src/main/java/com/threewks/thundr/view/jsp/JspViewResolver.java
@@ -1,5 +1,6 @@
 package com.threewks.thundr.view.jsp;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
@@ -10,10 +11,22 @@ import com.threewks.thundr.view.ViewResolutionException;
 import com.threewks.thundr.view.ViewResolver;
 
 public class JspViewResolver implements ViewResolver<JspView> {
+	/**
+	 * These values will be available in the model for every jsp resolved by this resolver.
+	 * An individual JspView will override the values in this map if it contains an entry with the same key.
+	 * 
+	 * This can be useful for setting values that apply to every page, or are extremely common. For example,
+	 * application environment, domain or version number.
+	 */
+	private Map<String, Object> globalModel = new HashMap<String, Object>();
+
 	@Override
 	public void resolve(HttpServletRequest req, HttpServletResponse resp, JspView viewResult) {
 		try {
 			Map<String, Object> model = viewResult.getModel();
+			for (Map.Entry<String, Object> modelEntry : globalModel.entrySet()) {
+				req.setAttribute(modelEntry.getKey(), modelEntry.getValue());
+			}
 			for (Map.Entry<String, Object> modelEntry : model.entrySet()) {
 				req.setAttribute(modelEntry.getKey(), modelEntry.getValue());
 			}
@@ -23,5 +36,17 @@ public class JspViewResolver implements ViewResolver<JspView> {
 		} catch (Exception e) {
 			throw new ViewResolutionException(e, "Failed to resolve JSP view %s", viewResult);
 		}
+	}
+
+	public Object addToGlobalModel(String key, Object value) {
+		return globalModel.put(key, value);
+	}
+
+	public Object removeFromGlobalModel(String key) {
+		return globalModel.remove(key);
+	}
+
+	public void addAllToGlobalModel(Map<String, Object> entries) {
+		globalModel.putAll(entries);
 	}
 }
--- src/test/java/com/threewks/thundr/view/jsp/JspViewResolverTest.java
+++ src/test/java/com/threewks/thundr/view/jsp/JspViewResolverTest.java
@@ -1,13 +1,14 @@
 package com.threewks.thundr.view.jsp;
 
 import static com.atomicleopard.expressive.Expressive.*;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 import java.util.Map;
 
 import org.junit.Test;
 
+import com.atomicleopard.expressive.Expressive;
 import com.threewks.thundr.test.mock.servlet.MockHttpServletRequest;
 import com.threewks.thundr.test.mock.servlet.MockHttpServletResponse;
 
@@ -29,5 +30,30 @@ public class JspViewResolverTest {
 		resolver.resolve(req, resp, new JspView("view.jsp", model));
 		assertThat(req.getAttribute("attribute1"), is((Object) "String val"));
 		assertThat(req.getAttribute("attribute2"), is((Object) list("Other", "Stuff")));
+	}
+
+	@Test
+	public void shouldAddAllGlobalModelAttributesAsRequestAttributes() {
+		resolver.addToGlobalModel("key 1", "value 1");
+		resolver.addAllToGlobalModel(Expressive.<String, Object> map("key 2", "value 2", "key 3", "value 3"));
+		resolver.resolve(req, resp, new JspView("view.jsp", Expressive.<String, Object> map()));
+		assertThat(req.getAttribute("key 1"), is((Object) "value 1"));
+		assertThat(req.getAttribute("key 2"), is((Object) "value 2"));
+		assertThat(req.getAttribute("key 3"), is((Object) "value 3"));
+	}
+
+	@Test
+	public void shouldAllowModelAttributesToOverrideGlobalModelAttributes() {
+		resolver.addToGlobalModel("key 1", "value 1");
+		resolver.resolve(req, resp, new JspView("view.jsp", Expressive.<String, Object> map("key 1", "some other value")));
+		assertThat(req.getAttribute("key 1"), is((Object) "some other value"));
+	}
+
+	@Test
+	public void shouldAllowRemovalOfGlobalModelAttributes() {
+		resolver.addToGlobalModel("key 1", "value 1");
+		resolver.removeFromGlobalModel("key 1");
+		resolver.resolve(req, resp, new JspView("view.jsp", Expressive.<String, Object> map()));
+		assertThat(req.getAttribute("key 1"), is(nullValue()));
 	}
 }
