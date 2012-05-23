3da4ddce349694581c99ad99c34acf242abcaa01
Nick Okunew <nick@atomicleopard.com>
2012-05-24 09:52:15 +1000

    Nick O: Added based string maniupulation functions for el

---
+++ src/main/java/com/threewks/thundr/view/jsp/el/StringFunctions.java
@@ -0,0 +1,22 @@
+package com.threewks.thundr.view.jsp.el;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+
+public class StringFunctions {
+	public static String upperCase(Object arg) {
+		return arg == null ? null : StringUtils.upperCase(arg.toString());
+	}
+
+	public static String lowerCase(Object arg) {
+		return arg == null ? null : StringUtils.lowerCase(arg.toString());
+	}
+
+	public static String sentenceCase(Object arg) {
+		return arg == null ? null : StringUtils.capitalize(arg.toString());
+	}
+
+	public static String capitalise(Object arg) {
+		return arg == null ? null : WordUtils.capitalizeFully(arg.toString());
+	}
+}
