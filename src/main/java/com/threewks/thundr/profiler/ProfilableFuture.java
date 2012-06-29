236bf3ef777373a1b9e0a7b86deee4745717ed54
Nick Okunew <nick@atomicleopard.com>
2012-06-29 18:01:28 +1000

    Nick O: missed a class checkin on some profiling stuff

---
+++ src/main/java/com/threewks/thundr/profiler/ProfilableFuture.java
@@ -0,0 +1,73 @@
+package com.threewks.thundr.profiler;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class ProfilableFuture<T> implements Future<T> {
+	public Profiler profiler;
+	public Future<T> delegate;
+	public String category;
+	public String data;
+
+	public ProfilableFuture(String category, String data, Profiler profiler, Future<T> delegate) {
+		super();
+		this.profiler = profiler;
+		this.delegate = delegate;
+		this.category = category;
+		this.data = data;
+	}
+
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		return delegate.cancel(mayInterruptIfRunning);
+	}
+
+	public T get() throws InterruptedException, ExecutionException {
+		UUID key = profiler.start(Profiler.CategoryHttp, data);
+		try {
+			T t = delegate.get();
+			profiler.end(key);
+			return t;
+		} catch (InterruptedException e) {
+			profiler.end(key, ProfileEventStatus.Failed);
+			throw e;
+		} catch (ExecutionException e) {
+			profiler.end(key, ProfileEventStatus.Failed);
+			throw e;
+		} catch (RuntimeException e) {
+			profiler.end(key, ProfileEventStatus.Failed);
+			throw e;
+		}
+	}
+
+	public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+		UUID key = profiler.start(Profiler.CategoryHttp, data);
+		try {
+			T t = delegate.get(timeout, unit);
+			profiler.end(key);
+			return t;
+		} catch (InterruptedException e) {
+			profiler.end(key, ProfileEventStatus.Failed);
+			throw e;
+		} catch (ExecutionException e) {
+			profiler.end(key, ProfileEventStatus.Failed);
+			throw e;
+		} catch (TimeoutException e) {
+			profiler.end(key, ProfileEventStatus.Timeout);
+			throw e;
+		} catch (RuntimeException e) {
+			profiler.end(key, ProfileEventStatus.Failed);
+			throw e;
+		}
+	}
+
+	public boolean isCancelled() {
+		return delegate.isCancelled();
+	}
+
+	public boolean isDone() {
+		return delegate.isDone();
+	}
+}
