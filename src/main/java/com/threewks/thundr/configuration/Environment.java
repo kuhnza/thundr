5ce0799e359be78da165b0e140551ba2b6817e85
Sean Elias <seanelias@ninl1007866.news.newslimited.local>
2012-11-05 18:02:50 +1100

    sean: refactored url appversion into Environment

--- src/main/java/com/threewks/thundr/configuration/Environment.java
+++ src/main/java/com/threewks/thundr/configuration/Environment.java
@@ -1,5 +1,9 @@
 package com.threewks.thundr.configuration;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.appengine.api.utils.SystemProperty;
+
 public class Environment {
 	public static Environment Production = new Environment("Production");
 	public static Environment Development = new Environment("Development");
@@ -14,4 +18,25 @@ public class Environment {
 	public String getEnvironment() {
 		return environment;
 	}
+
+	public static boolean isProduction() {
+		return Production.getEnvironment() == SystemProperty.Environment.environment.get();
+	}
+
+	/**
+	 * @return The application version with dot separator appended for
+	 *         production environment otherwise returns empty string
+	 */
+	public static String applicationVersion() {
+		// The version identifier for the current application version.
+		// Result is of the form <major>.<minor> where <major> is the
+		// version name supplied at deploy time
+		// and <minor> is a timestamp value maintained by App Engine
+		if (isProduction() && SystemProperty.applicationVersion.get().contains(".")) {
+			return StringUtils.substringBefore(SystemProperty.applicationVersion.get(), ".").concat(".");
+		}
+
+		return "";
+	}
+
 }
