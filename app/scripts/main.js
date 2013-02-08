386c33d6ac79ac5dbadf38bb5d1ae0e0bbc36ad3
JaapRood <jaap@jaaprood.nl>
2013-02-08 13:52:36 +1000

    Started reworking the navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,24 @@
 require.config({
-  shim: {
-  },
+	shim: {},
 
-  paths: {
-    jquery: 'vendor/jquery.min'
-  }
+	paths: {
+			jquery: 'vendor/jquery.min'
+	}
 });
  
-require(['app'], function(app) {
-  // use app here
-  console.log(app);
+require([], function() {
+	'use strict';
+
+	$(function() {
+		// If we cross link pages to a collapsed accordian, open the entry of interest.
+		// We'll also open any collapsed accordian section within the same page
+		if (location.hash) { $(location.hash + '.collapse').collapse('show'); }
+		$(window).bind('hashchange', function() {
+			if (location.hash) { $(location.hash + '.collapse').collapse('show'); }
+		});
+
+		$('.dropdown-toggle').dropdown();
+		$('.collapse').collapse();
+	});
+	
 });
